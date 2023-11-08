192e18bd7fc0422204466d856267aed219c6c6d1
jcorreaw <juninhocorrea2014@gmail.com>
2023-11-08 15:46:48 -0300

    media entre doios valores

---
+++ variaveisETipos/exercicios/MediaEntreDoisValores.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+void main(){
+
+    setlocale(LC_ALL, "");
+
+    float nota1, nota2, media;
+    printf("Digite a primeira nota:");
+    scanf("%f", &nota1);
+
+    printf("Digite a segunda nota:");
+    scanf("%f", &nota2);
+
+    media = (nota1 + nota2) / 2;
+
+    printf("A media de %f e %f é: %f", nota1, nota2, media);
+}
+
