102b9c5553e4c14f27f71d9d75834ab387090c87
jcorreaw <juninhocorrea2014@gmail.com>
2023-11-16 13:09:05 -0300

    Condicionais com Char

---
+++ condicionais/estudos/CondicionaisComChar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Função principal do programa
+void main(){
+
+    //Definindo Variáveis
+    char letra = 'x';
+
+    //Condicional Simples
+    if(letra == 'x'){
+        printf("\nA letra eh x.");
+    }
+
+    //Código em ASCII
+    printf("\nCodigo da letra = %d", letra);
+
+    //Comparando Código ASCII
+    if(letra == 120){
+         printf("\n A letra eh x.");
+    }
+}
