5c09d63cac348123f7205d13d71e788f94bf070b
jcorreaw <juninhocorrea2014@gmail.com>
2023-11-24 10:20:22 -0300

    Criar e Acessar Strings

---
+++ vetoresStringsMatrizes/estudos/CriarEAcessarStrings.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(){
+
+    //Variáveis
+    char palavra[10];
+
+    //Instrução
+    printf("Digite uma palavra: ");
+
+    //Limpa o Buffer
+    setbuf(stdin, 0);
+
+    //Lê a String
+    fgets(palavra, 255, stdin);
+
+    //Limpa as casas não utilizadas
+    palavra[strlen(palavra)-1] = '\0';
+
+    //Imprime na tela
+    printf("%s", palavra);
+
+}
